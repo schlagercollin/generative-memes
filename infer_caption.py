9d7aa0f7a79ba200cecbe004604e5faecc1db03b
Harry Mellsop <harrymellsop@gmail.com>
2022-02-16 13:03:51 +1300

    Some seriously cursed inference code, only had 20 mins so excuse... everything

---
+++ infer_caption.py
@@ -0,0 +1,89 @@
+import torch
+import os
+
+from dataset import MemeCaptionDataset
+from captionmodel import EncoderCNN, DecoderRNN
+
+from settings import caption_batch_size, workers
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+CKPT_PATH = "./caption-model-ckpts"
+
+def infer():
+    dataset = MemeCaptionDataset()
+    vocab_size = len(dataset.itos)
+
+    encoder = EncoderCNN().to(device)
+    decoder = DecoderRNN(
+        embed_size=1024, 
+        hidden_size=1024, 
+        vocab_size=vocab_size
+    ).to(device)
+
+    test_data_loader = iter(torch.utils.data.DataLoader(
+        dataset,
+        batch_size=caption_batch_size,
+        shuffle=True, 
+        num_workers=workers
+    ))
+
+    print("Loaded dataset; now loading model")
+
+    encoder.load_state_dict(
+        torch.load(os.path.join(CKPT_PATH, 'encoder-%d.pth' % 440), map_location=torch.device(device))
+    )
+
+    decoder.load_state_dict(
+        torch.load(os.path.join(CKPT_PATH, 'decoder-%d.pth' % 440), map_location=torch.device(device))
+    )
+    encoder.eval()
+    decoder.eval()
+
+    print("Successfully loaded model.")
+
+    for i_step, (img, caption) in enumerate(test_data_loader):
+        img = img.to(device)
+        caption = caption.to(device)
+
+        features = encoder(img)
+        outputs = decoder(features, caption)
+
+        predictions = pred_vec_to_text(outputs, dataset)
+
+        ground_truth_sentences = gt_vec_to_text(caption, dataset)
+
+        for i in range(outputs.shape[0]):
+            print("Predicted: ", predictions[i])
+            print("Ground truth: ", ground_truth_sentences[i])
+            print('\n\n\n')
+
+        break
+
+def gt_vec_to_text(caption, dataset):
+    sentences = []
+    for i in range(caption.shape[0]):
+        sentence = []
+        for j in range(caption.shape[1]):
+            sentence.append(dataset.itos[caption[i, j]])
+        sentences.append(sentence)
+
+    return sentences
+
+def pred_vec_to_text(outputs, dataset):
+    sentences = []
+    print(outputs.shape)
+    max_elements = torch.argmax(outputs, axis=2)
+    print(max_elements.shape)
+    print(max_elements)
+
+    for prediction in range(max_elements.shape[0]):
+        sentence = []
+        for i in range(max_elements.shape[1]):
+            sentence.append(dataset.itos[max_elements[prediction, i]])
+        sentences.append(sentence)
+
+    return sentences
+
+if __name__ == "__main__":
+    infer()
--- traincaption.py
+++ traincaption.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torchvision import models
 import sys
 import os
 from dataset import MemeCaptionDataset
@@ -8,64 +7,12 @@ import numpy as np
 
 from settings import caption_batch_size, workers, caption_num_epochs, caption_save_every
 
+from captionmodel import EncoderCNN, DecoderRNN
+
 CKPT_PATH = "./caption-model-ckpts"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class EncoderCNN(nn.Module):
 
-    def __init__(self, embed_size = 1024):
-        super(EncoderCNN, self).__init__()
-        self.densenet = models.densenet121(pretrained=True)
-        self.densenet.classifier = nn.Linear(in_features=1024, out_features=1024)
-        self.embed = nn.Linear(in_features=1024, out_features=embed_size)
-        self.dropout = nn.Dropout(p=0.5)
-        self.prelu = nn.PReLU()
-        
-    def forward(self, images):
-        densenet_outputs = self.dropout(self.prelu(self.densenet(images)))
-        return self.embed(densenet_outputs)
-
-
-class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        super(DecoderRNN, self).__init__()
-        
-        self.embed_size = embed_size
-        self.hidden_size = hidden_size
-        self.vocab_size = vocab_size
-        
-        self.lstm_cell = nn.LSTMCell(input_size=embed_size, hidden_size=hidden_size)
-    
-        self.fc_out = nn.Linear(in_features=self.hidden_size, out_features=self.vocab_size)
-    
-        self.embed = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embed_size)
-    
-        self.softmax = nn.Softmax(dim=1)
-    
-    def forward(self, features, captions):
-  
-        batch_size = features.size(0)
-        
-        hidden_state = torch.zeros((batch_size, self.hidden_size)).to(device)
-        cell_state = torch.zeros((batch_size, self.hidden_size)).to(device)
-    
-        outputs = torch.empty((batch_size, captions.size(1), self.vocab_size)).to(device)
-
-        captions_embed = self.embed(captions)
-        
-        for t in range(captions.size(1)):
-
-            if t == 0:
-                hidden_state, cell_state = self.lstm_cell(features, (hidden_state, cell_state))
-                
-            else:
-                hidden_state, cell_state = self.lstm_cell(captions_embed[:, t, :], (hidden_state, cell_state))
-            
-            out = self.fc_out(hidden_state)
-            
-            outputs[:, t, :] = out
-    
-        return outputs
 
 
 def train():
