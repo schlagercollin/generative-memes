d954aa352b6a753485d3704ec5a6acb2cfe35f4d
Collin Schlager <schlagercollin@gmail.com>
2022-02-23 13:46:48 -0800

    Updating inference functions and refactoring

--- infer_full.py
+++ infer_full.py
@@ -14,127 +14,35 @@ import numpy as np
 import cv2
 from PIL import Image
 
+import utils
+
 to_pil = torchvision.transforms.ToPILImage(mode='RGB')
 
-def create_meme(generator,
-        encoder,
-        decoder,
-        data_loader,
-        device,
-        dataset,
-        random_noise=None
-    ):
-    """Runs the full inference pipeline for the baseline (separate components)
-    architecture.
-
-    Args:
-        generator: the generator model
-        encoder: the caption encoder model
-        decoder: the caption decoder model
-        data_loader: the data loader
-        device: torch device
-        dataset: the meme caption and image dataset
-        random_noise: random noise to pass to generator Defaults to None.
-
-    Returns:
-        image with overlayed, generated text caption in meme style
-    """
-
-    if random_noise is None:
-        random_noise = torch.nn.init.trunc_normal_(
-            torch.zeros((1, 100)),
-            a=-2,
-            b=2,
-        )
+def generate_background(generator, device, noise=None, truncated_normal=False):
+    if noise is None:
+        if truncated_normal:
+            noise = torch.nn.init.trunc_normal_(
+                torch.zeros((1, 100)),
+                a=-2,
+                b=2,
+            )
+        else:
+            noise = generator.sampler(1, device)[0]
     
+    generator_out = generator.forward(noise)
+    return generator_out
 
-    meme_background = generator.forward(*generator.sampler(1, 'cpu'))
+def generate_caption(generator_output, encoder, decoder, data_loader, device, dataset):
 
-    features = encoder(meme_background)
+    features = encoder(generator_output)
 
     _, dummy_cap = next(data_loader)
     dummy_cap = dummy_cap[:1].to(device)
-    caption = decoder(features, dummy_cap)
+    caption_tensor = decoder(features, dummy_cap)
+    text_caption = " ".join([word for word in pred_vec_to_text(caption_tensor, dataset)[0] if word != "<UNK>"])
+    
+    return text_caption
 
-    cap = " ".join([word for word in pred_vec_to_text(caption, dataset)[0] if word != "<UNK>"])
-    image = meme_background[0].permute(1, 2, 0).detach().numpy()
-    image = cv2.normalize(image, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 
-    image = image.astype(np.uint8)
-    image = Image.fromarray(image)
-    image = image.resize((256, 256))
-
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("SF-Compact-Display-Bold.otf", 16)
-    # draw.text((0, 0),"Sample Text",(255,255,255),font=font)
-
-    def drawTextWithOutline(text, x, y):
-        draw.text((x-2, y-2), text,(0,0,0),font=font)
-        draw.text((x+2, y-2), text,(0,0,0),font=font)
-        draw.text((x+2, y+2), text,(0,0,0),font=font)
-        draw.text((x-2, y+2), text,(0,0,0),font=font)
-        draw.text((x, y), text, (255,255,255), font=font)
-
-    def drawText(img, text, pos):
-        text = text.upper()
-        w, h = draw.textsize(text, font) # measure the size the text will take
-
-        lineCount = 1
-        if w > img.width:
-            lineCount = int(round((w / img.width) + 1))
-
-        lines = []
-        if lineCount > 1:
-
-            lastCut = 0
-            isLast = False
-            for i in range(0,lineCount):
-                if lastCut == 0:
-                    cut = (len(text) // lineCount) * i
-                else:
-                    cut = lastCut
-
-                if i < lineCount-1:
-                    nextCut = (len(text) // lineCount) * (i+1)
-                else:
-                    nextCut = len(text)
-                    isLast = True
-
-                # make sure we don't cut words in half
-                if nextCut == len(text) or text[nextCut] == " ":
-                    pass
-                else:
-                    while text[nextCut] != " ":
-                        nextCut += 1
-
-                line = text[cut:nextCut].strip()
-
-                # is line still fitting ?
-                w, h = draw.textsize(line, font)
-                if not isLast and w > img.width:
-                    nextCut -= 1
-                    while text[nextCut] != " ":
-                        nextCut -= 1
-
-                lastCut = nextCut
-                lines.append(text[cut:nextCut].strip())
-
-        else:
-            lines.append(text)
-
-        lastY = -h
-        if pos == "bottom":
-            lastY = img.height - h * (lineCount+1) - 10
-
-        for i in range(0, lineCount):
-            w, h = draw.textsize(lines[i], font)
-            x = img.width/2 - w/2
-            y = lastY + h
-            drawTextWithOutline(lines[i], x, y)
-            lastY = y
-
-    drawText(image, cap, "top")
-
-    return image
 
     
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import torch
 import torchvision
 import torchvision.transforms as T 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
 
 def tensor_to_image(output: torch.Tensor, ncol: int=4, padding: int=2) -> Image:
     """Convert the tensor-based output from a Generator into a PIL image.
@@ -18,6 +19,7 @@ def tensor_to_image(output: torch.Tensor, ncol: int=4, padding: int=2) -> Image:
     """
     out = torchvision.utils.make_grid(output, nrow=ncol, normalize=True, padding=padding)
     return T.ToPILImage()(out)
+
 
 def interpolate(a: torch.Tensor, b: torch.Tensor, num_steps: int=64) -> torch.Tensor:
     """Linearly interpolate between tensors A and B with num_steps steps.
@@ -37,3 +39,85 @@ def interpolate(a: torch.Tensor, b: torch.Tensor, num_steps: int=64) -> torch.Te
         raise ValueError(f"a and b need the same shape. got {a.shape} and {b.shape}.")
     
     return torch.stack([torch.lerp(a, b, weight) for weight in torch.linspace(0, 1, num_steps)])
+
+
+class Meme:
+    """
+    Adapted from: 
+    https://stackoverflow.com/questions/63498671/create-a-captioned-meme-using-python-and-pil
+    """
+    basewidth = 1200            #Width to make the meme
+    fontBase = 100              #Font size
+    letSpacing = 9              #Space between letters
+    fill = (255, 255, 255)      #TextColor
+    stroke_fill = (0,0,0)       #Color of the text outline
+    lineSpacing = 10            #Space between lines
+    stroke_width=9              #How thick the outline of the text is
+    fontfile = './Impact.ttf'   #Points to the font file (local to GitHub)
+
+    def __init__(self, caption, image):
+        self.img = self.createImage(image)
+        self.d = ImageDraw.Draw(self.img)
+
+        self.splitCaption = textwrap.wrap(caption, width=20)  # The text can be wider than the img. If thats the case split the text into multiple lines
+        self.splitCaption.reverse()                           # Draw the lines of text from the bottom up
+
+        fontSize = self.fontBase+10 if len(self.splitCaption) <= 1 else self.fontBase   #If there is only one line, make the text a bit larger
+        self.font = ImageFont.truetype(font=self.fontfile, size=fontSize)
+        # self.shadowFont = ImageFont.truetype(font='./impact.ttf', size=fontSize+10)
+
+    def draw(self):
+        '''
+        Draws text onto this objects img object
+        :return: A pillow image object with text drawn onto the image
+        '''
+        (iw, ih) = self.img.size
+        (_, th) = self.d.textsize(self.splitCaption[0], font=self.font) #Height of the text
+        # y = (ih - (ih / 10)) - (th / 2) #The starting y position to draw the last line of text. Text in drawn from the bottom line up
+        # y = (0 + (ih / 10)) + (th / 2)  # Collin edit: flip signs to start text at top
+        y = 5  # Collin edit: this starting point seems reasonable
+
+        for cap in self.splitCaption:   #For each line of text
+            (tw, _) = self.d.textsize(cap, font=self.font)  # Getting the position of the text
+            x = ((iw - tw) - (len(cap) * self.letSpacing))/2  # Center the text and account for the spacing between letters
+
+            self.drawLine(x=x, y=y, caption=cap)
+            # y = y - th - self.lineSpacing  # Next block of text is higher up
+            y = y + th + self.lineSpacing # Collin edit: flip signs to start text at top
+
+        wpercent = ((self.basewidth/2) / float(self.img.size[0]))
+        hsize = int((float(self.img.size[1]) * float(wpercent)))
+        return self.img.resize((int(self.basewidth/2), hsize))
+
+    def createImage(self, img):
+        '''
+        Resizes the image to a resonable standard size
+        :param image: Path to an image file or PIL Image
+        :return: A pil image object
+        '''
+        if type(img) == str:
+            img = Image.open(img)
+        wpercent = (self.basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        return img.resize((self.basewidth, hsize))
+
+    def drawLine(self, x, y, caption):
+        '''
+        The text gets split into multiple lines if it is wider than the image. This function draws a single line
+        :param x: The starting x coordinate of the text
+        :param y: The starting y coordinate of the text
+        :param caption: The text to write on the image
+        :return: None
+        '''
+        for idx in range(0, len(caption)):  #For each letter in the line of text
+            char = caption[idx]
+            w, h = self.font.getsize(char)  #width and height of the letter
+            self.d.text(
+                (x, y),
+                char,
+                fill=self.fill,
+                stroke_width=self.stroke_width,
+                font=self.font,
+                stroke_fill=self.stroke_fill
+            )  # Drawing the text character by character. This way spacing can be added between letters
+            x += w + self.letSpacing #The next character must be drawn at an x position more to the right
